a2379aa63973c7f3d6f3ac7ca9ebe7f1955e369e
Jibin <jibinmathews7@gmail.com>
2018-07-08 15:09:09 +0530

    Adding helm apply

--- .circleci/helm-apply.sh
+++ .circleci/helm-apply.sh
@@ -5,3 +5,15 @@
 aws s3 cp s3://bc-kubeconfig/config ~/.kube/config
 
 helm init --client-only
+
+setVariables="NODE_ENV=${NODE_ENV},image=${IMAGE_NAME},commitHash=${COMMIT_HASH}"
+releaseName="blockcluster-app-${NODE_ENV}"
+
+helm --debug \
+  --kube-context "k8s-${CLUSTER_PREFIX}.blockcluster.io" \
+  upgrade \
+  --install \
+  --set ${setVariables} \
+  $releaseName \
+  ./helm-chart
+
