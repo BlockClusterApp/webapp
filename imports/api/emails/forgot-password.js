cc5bfe640c7894431082252c7685fe7e8df9ed9a
Jibin <jibinmathews7@gmail.com>
2018-07-13 15:37:10 +0530

    Adding server functions

--- imports/api/emails/forgot-password.js
+++ imports/api/emails/forgot-password.js
@@ -1,4 +1,4 @@
-import { EmailVerification } from "../../collections/email-verification";
+import { PasswordResetRequest } from "../../collections/password-reset";
 import Email from "./email-sender";
 import {
   generateRandomString,
@@ -6,8 +6,9 @@ import {
   getEJSTemplate
 } from "../../modules/helpers/server";
 
+const PasswordResetter = {};
 
-Verifier.sendEmailVerification = async function(user) {
+PasswordResetter.resetLinkEmail = async function(user) {
   const email = user.emails[0].address;
   const uniqueString = generateRandomString(`${email}-${new Date().toString()}`);
   const link = generateCompleteURLForPasswordReset(uniqueString);
@@ -32,7 +33,7 @@ Verifier.sendEmailVerification = async function(user) {
   await Email.sendEmail(emailProps);
 
   // TODO: Wrapper around callback insert for async await to work
-  const reply = EmailVerification.insert({
+  const reply = PasswordResetRequest.insert({
     accountId: user._id,
     emailId: email,
     uniqueToken: uniqueString
@@ -41,13 +42,12 @@ Verifier.sendEmailVerification = async function(user) {
   return true;
 };
 
-Verifier.validateToken = function(token, emailId) {
+PasswordResetter.validateToken = function(token) {
   return new Promise(async (resolve, reject) => {
-    let emailVerificationDoc;
+    let passwordResetRequest;
     try {
-      emailVerificationDoc = EmailVerification.find({
+      passwordResetRequest = PasswordResetRequest.find({
         uniqueToken: token,
-        emailId,
         active: true
       }).fetch()[0];
     } catch (err) {
@@ -55,43 +55,40 @@ Verifier.validateToken = function(token, emailId) {
       return resolve(false);
     }
 
-    if(!emailVerificationDoc) {
+    if(!passwordResetRequest) {
       return resolve(false)
     }
 
-    const accountId = emailVerificationDoc.accountId;
-    const updateResult = Meteor.users.update(
-      {
-        _id: accountId,
-        "emails.address": emailVerificationDoc.emailId,
-      },
-      {
-        $set: {
-          "emails.$.emailVerified": true
-        }
-      }
-    );
-        
-    const emailUpdateResult = EmailVerification.update(
-            {
-              _id: emailVerificationDoc._id
-            },
-            {
-              $set: {
-                active: false
-              }
-            }
-          ); 
-
-      return resolve(true);
+    return resolve(true);
   });
 };
+
+PasswordResetter.changePassword = async function(token, password) {
+  const passwordResetRequest = PasswordResetRequest.find({
+    uniqueToken: token,
+    active: true
+  }).fetch()[0];
+  if(!passwordResetRequest){
+    return false;
+  }
+  Accounts.setPassword(passwordResetRequest.accountId, `${password}`);
+  PasswordResetRequest.update({
+    _id: passwordResetRequest._id
+  }, {
+    $set: {
+      active: false
+    }
+  });
+  return true;
+}
 
 
 
 Meteor.methods({
-    async emailVerification (token, email){
-        const result = await Verifier.validateToken(token, email);
-        return result;
-    }
+  async verifyResetPasswordLink(token) {
+    return PasswordResetter.validateToken(token);
+  },
+  async changePassword(token, password) {
+    return PasswordResetter.changePassword(token, password);
+  }
 })
