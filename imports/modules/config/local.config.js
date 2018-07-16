872d8e2d6d12a4e3464f8595f31992bcf0927c2b
Jibin <jibinmathews7@gmail.com>
2018-07-16 13:38:35 +0530

    Changing redis host

--- imports/modules/config/local.config.js
+++ imports/modules/config/local.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     sendgridApi: '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',
-    redisHost: "52.43.83.54",
-    redisPort: 30296,
+    redisHost: "18.237.94.215",
+    redisPort: 30645,
     apiHost: "localhost:3000",
     kubeRestApiHost: "http://34.208.153.18:8000",
     firewallPort: "31988",
