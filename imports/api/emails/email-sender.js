d40adfa047a18f3a2ac3598f038526fe22472947
Jibin <jibinmathews7@gmail.com>
2018-07-17 17:20:05 +0530

    Sendgrid missed config

--- imports/api/emails/email-sender.js
+++ imports/api/emails/email-sender.js
@@ -1,4 +1,5 @@
 import { Email } from "../../collections/emails";
+import Config from '../../modules/config/server';
 // import nodemailer from "nodemailer";
 
 import sg from "@sendgrid/mail";
@@ -6,7 +7,7 @@ import sg from "@sendgrid/mail";
 const sendEmail = function(emailOptions) {
   return new Promise( (resolve, reject) => {
     process.nextTick(async () => {
-      sg.setApiKey(process.env.SENDGRID_API_KEY);
+      sg.setApiKey(Config.sendgridAPIKey);
       const res = await sg.send(emailOptions);
       const insertResult = Email.insert(emailOptions);
       resolve();
