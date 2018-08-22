732d7b191b2f9655304e2a22f2c4fd5ad38e56a1
saikat chakrabortty <saikatchakrabortty2@gmail.com>
2018-08-22 14:12:26 +0530

    Create vouchers in Bulk mode [BACKEND]

--- imports/api/network/voucher.js
+++ imports/api/network/voucher.js
@@ -1,4 +1,5 @@
-import Vouchers from '../../collections/vouchers/voucher';
+import Vouchers from "../../collections/vouchers/voucher";
+import moment from "moment";
 
 const Voucher = {};
 
@@ -12,13 +13,97 @@ Voucher.validate = async function(voucherCode) {
     }
   }).fetch()[0];
 
-  if(!voucher) {
+  if (!voucher) {
     throw new Meteor.Error("Invalid or expired voucher");
   }
 
   return voucher;
+};
+
+const insertVoucher = async savable_doc => {
+  Vouchers.insert(savable_doc);
+  return true;
+};
+
+/**
+ * @param { noOfVouchers*, networkConfig*, expiryDate*, isDiskChangeable*, discountedDays*, claimed*, active*} payload
+ * @param { cpu: Number, ram: Number, disk: Number } payload.networkConfig
+ */
+Voucher.create = async function(payload) {
+  let voucher_codes = await generateVouchers(payload.noOfVouchers, 6); //lets keep it by default 6 for now
+
+  let savabl_doc = [];
+  voucher_codes.forEach(voucher => {
+    savabl_doc.push({
+      createdAt: new Date(),
+      usablity: {
+        recurring: payload.usablity.recurring || false,
+        no_months: payload.usablity.no_months || 0
+      },
+      availability: {
+        for_all: payload.availability.for_all || false,
+        email_ids: payload.availability.email_ids || []
+      },
+      discount: {
+        value: payload.discount.value || 0,
+        percent: payload.discount.value || false
+      },
+      code: voucher,
+      networkConfig: payload.networkConfig,
+      expiryDate:
+        payload.expiryDate ||
+        moment()
+          .add(30, "days")
+          .toDate(), //lets take by default 30days
+      isDiskChangeable: payload.isDiskChangeable || false,
+      discountedDays: payload.discountedDays || 0,
+      claimed: payload.claimed || false
+    });
+  });
+  return insertVoucher(savabl_doc);
+};
+/**
+ *
+ *
+ * @param {Number*} items
+ * @param {Number*} size
+ * @returns {Promise*}
+ */
+function generateVouchers(items, size) {
+  let voucherArray = [];
+  return new Promise((resolve, reject) => {
+    let flag = 0;
+    for (i = 1; i <= items; i++) {
+      console.log(i);
+      voucherArray.push(
+        Math.round(Math.pow(36, size + 1) - Math.random() * Math.pow(36, size))
+          .toString(36)
+          .slice(1)
+          .toUpperCase()
+      );
+      if (voucherArray.length + flag == items) {
+        const existing_codes = Vouchers.find({ code: { $in: voucherArray } })
+          .select({ code: 1 })
+          .fetch();
+        if (existing_codes.length) {
+          existing_codes.forEach(element => {
+            let index = voucherArray.indexOf(element.code);
+            if (index > -1) {
+              voucherArray.splice(index, 1);
+              items++;
+              flag++;
+            }
+          });
+        } else {
+          return resolve(voucherArray);
+        }
+      }
+    }
+    return resolve(voucherArray);
+  });
 }
 
 Meteor.methods({
-  validateVoucher: Voucher.validate
+  validateVoucher: Voucher.validate,
+  CreateVoucher: Voucher.create
 });
--- imports/collections/vouchers/voucher.js
+++ imports/collections/vouchers/voucher.js
@@ -1,50 +1,67 @@
-import { Mongo } from 'meteor/mongo';
+import { Mongo } from "meteor/mongo";
 
-import SimpleSchema from 'simpl-schema';
+import SimpleSchema from "simpl-schema";
 
-import AttachBaseHooks from '../../modules/helpers/model-helpers';
+import AttachBaseHooks from "../../modules/helpers/model-helpers";
 
-const VoucherCollection = new Mongo.Collection(
-  "vouchers"
-);
+const VoucherCollection = new Mongo.Collection("vouchers");
 
 AttachBaseHooks(VoucherCollection);
 
-VoucherCollection.schema = new SimpleSchema({
-  createdAt: {
-    type: Date
+VoucherCollection.schema = new SimpleSchema(
+  {
+    createdAt: {
+      type: Date
+    },
+    updatedAt: {
+      type: Date
+    },
+    code: {
+      type: String,
+      required: true
+    },
+    expiryDate: {
+      type: Date
+    },
+    usablity: {
+      recurring: { type: Boolean }, //false means one time per user
+      no_months: { type: Number }
+    },
+    availability: {
+      for_all: { type: Boolean }, //false means available to all
+      email_ids: { type: Array }
+    },
+    discount: {
+      value: {
+        type: Number
+      },
+      percent: {
+        type: Boolean //false means consider the value as a flat amount.
+      }
+    },
+    networkConfig: {
+      type: Object
+    },
+    active: {
+      type: Boolean
+    },
+    isDiskChangeable: {
+      type: Boolean
+    },
+    discountedDays: {
+      type: Number
+    },
+    claimed: {
+      type: Boolean
+    },
+    claimedBy: {
+      type: String
+    },
+    claimedOn: {
+      type: Date
+    }
   },
-  updatedAt: {
-    type: Date
-  },
-  code: {
-    type: String,
-    required: true
-  },
-  expiryDate: {
-    type: Date
-  },
-  networkConfig: {
-    type: Object
-  },
-  active: {
-    type: Boolean
-  },
-  isDiskChangeable: {
-    type: Boolean
-  },
-  discountedDays: {
-    type: Number
-  },
-  claimed: {
-    type: Boolean
-  },
-  claimedBy: {
-    type: String
-  },
-  claimedOn: {
-    type: Date
-  }
-});
+  { timesta }
+);
 
 export default VoucherCollection;
