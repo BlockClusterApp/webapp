0f110ffda7f8943d2032c6d47464b32dad563e9b
Jibin <jibinmathews7@gmail.com>
2018-09-04 01:40:48 +0530

    Change forex, fix paisa issue and debit invoice

--- imports/api/payments/index.js
+++ imports/api/payments/index.js
@@ -2,6 +2,7 @@ import RazorPay from './payment-gateways/razorpay';
 import Forex from '../../collections/payments/forex';
 import PaymentRequests from '../../collections/payments/payment-requests';
 import { RZSubscription, RZPlan } from '../../collections/razorpay';
+const debug = require('debug')('api:payments')
 
 const Payments = {};
 
@@ -36,22 +37,27 @@ Payments.createRequest = async ({ paymentGateway, reason, amount, mode }) => {
       userId: Meteor.userId(),
       paymentGateway,
       reason,
-      amount: amount * 100,
+      amount: Math.round(amount * 100),
       conversionFactor
     });
-    return {paymentRequestId: insertResult, amount: amount * 100, display_amount: 1, display_currency: 'USD' };
+    const returnValue = {paymentRequestId: insertResult, amount: Math.round(amount * 100), display_amount: 1, display_currency: 'USD' };
+    debug('Payment create request | Debit RZP Options', returnValue);
+    return returnValue;
   }
 
 
   const display_amount = Number(amount / conversionFactor).toFixed(2);
 
-  return { paymentRequestId: insertResult, rzSubscriptionId: subscription.id, amount: amount * 100, display_amount, display_currency: 'USD' };
+  const returnValue = { paymentRequestId: insertResult, rzSubscriptionId: subscription.id, amount: amount * 100, display_amount, display_currency: 'USD' };
+  debug('Payment create request | Credit RZP Options', returnValue);
+  return returnValue;
 };
 
 Payments.getConversionToINRRate = async ({ currencyCode }) => {
   currencyCode = currencyCode || 'usd';
 
   const exchangeRates = Forex.find({}).fetch()[0];
+  debug('Get Conversion to INR | Forex ', exchangeRates);
   return Number(Number(exchangeRates[currencyCode.toLowerCase()]).toFixed(4));
 };
 
