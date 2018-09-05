89be1fae82c571686c0efab8e92792eabcde9575
Jibin <jibinmathews7@gmail.com>
2018-09-06 01:05:27 +0530

    Production notif only

--- .circleci/build-start-notification.sh
+++ .circleci/build-start-notification.sh
@@ -2,9 +2,11 @@
 
 URL="https://hooks.slack.com/services/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"
 
-curl -X POST $URL -H 'Content-type: application/json' --data """{
- \"text\":\"\` $CIRCLE_BRANCH \` deployment for \` $CIRCLE_PROJECT_REPONAME \` started by \` $CIRCLE_USERNAME \` - track progress here - https://circleci.com/workflow-run/$CIRCLE_WORKFLOW_ID \"
-}"""
-
+if [ "$CIRCLE_TAG" = "production" ] || [ "$CIRCLE_BRANCH" = "master" ];
+then
+  curl -X POST $URL -H 'Content-type: application/json' --data """{
+   \"text\":\"\` $CIRCLE_BRANCH \` deployment for \` $CIRCLE_PROJECT_REPONAME \` started by \` $CIRCLE_USERNAME \` - track progress here - https://circleci.com/workflow-run/$CIRCLE_WORKFLOW_ID \"
+  }"""
+fi
 exit 0;
 
