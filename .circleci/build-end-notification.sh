07ef1bc69e4bdf4209c5e0a67bf58b3566b32a25
Jibin <jibinmathews7@gmail.com>
2018-09-05 22:19:41 +0530

    Change slack text

--- .circleci/build-end-notification.sh
+++ .circleci/build-end-notification.sh
@@ -32,7 +32,7 @@ curl $CLIENT_URL \
 
 URL="https://hooks.slack.com/services/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"
 curl -X POST $URL -H 'Content-type: application/json' --data """{
-  \"text\":\" @here Deploying \` $CIRCLE_PROJECT_REPONAME \` to \` $CIRCLE_BRANCH \` finished \"
+  \"text\":\" Deploying \` $CIRCLE_PROJECT_REPONAME \` to \` $CIRCLE_BRANCH \` finished \"
 }"""
 
 exit 0;
