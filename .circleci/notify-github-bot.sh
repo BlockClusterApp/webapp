48a54d1debd2068483999ccd2932b0d2179b061d
Jibin <jibinmathews7@gmail.com>
2018-09-08 19:30:43 +0530

    Adding commit hook

--- .circleci/notify-github-bot.sh
+++ .circleci/notify-github-bot.sh
@@ -4,6 +4,8 @@ PR=${CIRCLE_PULL_REQUEST}
 
 if [[ -z ${PR} ]]
 then
+  CST=$(echo ${CIRCLE_SHA1} | base64)
+  curl -v "https://rp7epz6kpg.execute-api.ap-south-1.amazonaws.com/production/commit/tests-passed?sha=${CST}"; exit 0
   exit 0;
 fi
 
