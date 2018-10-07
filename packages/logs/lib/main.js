cf9741237ff8b2bcea24d5d37c137d07c9e82f0f
Jibin <jibinmathews7@gmail.com>
2018-10-07 16:21:40 +0530

    Adding blockcluster logs

---
+++ packages/logs/lib/main.js
@@ -0,0 +1,81 @@
+const winston = Npm.require('winston');
+
+var logger = console.log;
+
+function fetchFormat(message, tags) {
+  if (typeof tags === 'object') {
+    return {
+      message,
+      ...tags,
+      timestamp: new Date(),
+    };
+  } else {
+    return {
+      message,
+      tags,
+      timestamp: new Date(),
+    };
+  }
+}
+
+function initialize(settings) {
+  const finalOpts = {};
+  if (Meteor.isServer) {
+    finalOpts.level = 'info';
+    finalOpts.format = winston.format.json();
+    finalOpts.transports = [new winston.transports.Console()];
+    settings.logFiles = settings.logFiles || [];
+    settings.logFiles.forEach(logFile => {
+      const loggerOpts = {
+        filename: logFile.filename,
+      };
+      if(logFile.level) {
+        loggerOpts.level = logFile.level;
+      } else {
+        loggerOpts.level = 'info';
+      }
+      finalOpts.transports.push(
+        new winston.transports.File(loggerOpts)
+      );
+    });
+    logger = winston.createLogger(finalOpts);
+  } else {
+    logger = console.log;
+  }
+}
+
+function log(message, tags) {
+  logger.info(fetchFormat(message, tags));
+}
+
+function info(message, tags) {
+  if (Meteor.isServer) {
+    logger.info(fetchFormat(message, tags));
+  } else {
+    logger.info(message, tags);
+  }
+}
+
+function error(message, tags) {
+  if (Meteor.isServer) {
+    logger.error(fetchFormat(message, tags));
+  } else {
+    logger.error(message, tags);
+  }
+}
+
+function warn(message, tags) {
+  if (Meteor.isServer) {
+    logger.warn(fetchFormat(message, tags));
+  } else {
+    logger.warn(message, tags);
+  }
+}
+
+ElasticLogger = {
+  initialize: initialize,
+  log,
+  info,
+  warn,
+  error,
+};
