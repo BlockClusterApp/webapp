b9c8244ae533aac8cbb40cc7fc4c6f5ffddcc580
saikat <saikatchakrabortty2@gmail.com>
2018-10-15 14:26:27 +0530

    Added optional params

--- imports/ui/pages/admin/clients/ClientCreate.jsx
+++ imports/ui/pages/admin/clients/ClientCreate.jsx
@@ -2,27 +2,30 @@ import React, { Component } from 'react';
 import { withTracker } from 'meteor/react-meteor-data';
 import { withRouter } from 'react-router-dom';
 import notifications from '../../../../modules/notifications';
-import Vouchers from '../../../../collections/vouchers/voucher';
 import LaddaButton, { S, SLIDE_UP } from 'react-ladda';
 import Toggle from 'react-toggle';
 import 'react-toggle/style.css';
 import Axios from 'axios';
-import config from '../../../../modules/config/client'
+import CopyToClipboard from 'react-copy-to-clipboard'
+import config from '../../../../modules/config/client';
 
 Axios.defaults.baseURL = config.licensingMicroserviceBase;
 
-Axios.defaults.headers.common['x-access-key']=0+new Date().setHours(new Date().getHours(), 0,0,0).toString()+1+Date.now()+000;
+Axios.defaults.headers.common['x-access-key'] = 0 + new Date().setHours(new Date().getHours(), 0, 0, 0).toString() + 1 + Date.now() + 000;
 
 class ClientCreate extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-        clientName:'',
-        emailId:'',
-        phone:'',
-        genLicense:true,
-        expire:3
+      clientName: '',
+      emailId: '',
+      phone: '',
+      genLicense: true,
+      expire: 3,
+      clientMeta:'',
+      clientLogo:'',
+      serviceIncluded:{}
     };
   }
 
@@ -36,59 +39,64 @@ class ClientCreate extends Component {
       [e.target.name]: e.target.checked,
     });
   };
-  resetFrom = ()=>{
-    this.setState({
-        clientName:'',
-        emailId:'',
-        phone:'',
-        genLicense:true,
-        expire:3
-    },()=>{
+  resetFrom = () => {
+    this.setState(
+      {
+        clientName: '',
+        emailId: '',
+        phone: '',
+        genLicense: true,
+        expire: 3,
+      },
+      () => {
         notifications.success('resetting form');
-    });
+      }
+    );
   };
-  createClient =e =>{
+  createClient = e => {
     this.setState({
-        createClient_formSubmitError: '',
-        createClient_formloading: true,
+      createClient_formSubmitError: '',
+      createClient_formloading: true,
+    });
+    const data = {
+      clientDetails: {
+        clientName: this.state.clientName,
+        emailId: this.state.emailId,
+        phone: this.state.phone,
+      },
+      license: {
+        gen_license: this.state.genLicense,
+        expire: this.state.expire,
+      },
+    };
+
+    Axios.post('/client/create_client', data)
+      .then(saved => {
+        this.setState({
+          ['createClient_formloading']: false,
+          ['createClient_formSubmitError']: '',
+        });
+        console.log(saved);
+        this.resetFrom();
+        notifications.success('Client Added');
+      })
+      .catch(error => {
+        this.setState({
+          ['createClient_formloading']: false,
+          ['createClient_formSubmitError']: '',
+        });
+        console.log(error);
+        notifications.error('Error occured');
       });
-      const data = {
-            "clientDetails": {
-            "clientName": this.state.clientName,
-            "emailId": this.state.emailId,
-            "phone": this.state.phone
-          },
-          "license":{
-              "gen_license":this.state.genLicense,
-              "expire":this.state.expire
-          }
-      };
-      
-   Axios.post('/client/create_client',data).then(saved=>{
-    this.setState({
-        ['createClient_formloading']: false,
-        ['createClient_formSubmitError']: '',
-      });
-    console.log(saved);
-    this.resetFrom()
-    notifications.success('Client Added');
-   }).catch(error=>{
-    this.setState({
-        ['createClient_formloading']: false,
-        ['createClient_formSubmitError']: '',
-      });
-       console.log(error);
-    notifications.error('Error occured')
-   });
-  }
+  };
   render() {
     return (
-      <div className="content VoucherCreate">
+      <div className="content ClientCreate">
         <div className="m-t-20 container-fluid container-fixed-lg bg-white">
           <div className="row">
             <div className="card-block">
               <div className="form-group">
-              <label>Client Details</label>
+                <label>Client Details</label>
                 <div className="row">
                   <div className="col-md-4 form-input-group">
                     <label>Client Name</label>
@@ -105,19 +113,11 @@ class ClientCreate extends Component {
                   <div className="col-md-4 form-input-group">
                     <label>Client Email</label>
                     <span className="help"> e.g. joe@blockcluster.io </span>
-                    <input
-                      name="emailId"
-                      type="text"
-                      placeholder="Email"
-                      className="form-control"
-                      onChange={this.handleChanges.bind(this)}
-                      value={this.state.emailId}
-                      required
-                    />
+                    <input name="emailId" type="text" placeholder="Email" className="form-control" onChange={this.handleChanges.bind(this)} value={this.state.emailId} required />
                   </div>
                   <div className="col-md-4 form-input-group">
                     <label>Client Phone</label>
-                    <span className="help"> e.g. +1 8373886873  </span>
+                    <span className="help"> e.g. +1 8373886873 </span>
                     <input
                       name="phone"
                       type="text"
@@ -133,28 +133,58 @@ class ClientCreate extends Component {
                 <br />
                 <label>License</label>
                 <div className="row">
-                <div className="col-md-4 form-input-group">
+                  <div className="col-md-4 form-input-group">
                     <label>License Generate Now</label>
                     <Toggle name="genLicense" className="form-control" icons={false} checked={this.state.genLicense} onChange={this.handleChangesToggle.bind(this)} />
-                </div>{this.state.genLicense && (
-                  <div className="col-md-4 form-input-group">
-                    <label>License Expiry After </label>
-                    <span className="help">  [in Months]</span>
-                    <input
-                      name="expire"
-                      type="number"
-                      placeholder="no of months"
-                      className="form-control"
-                      onChange={this.handleChanges.bind(this)}
-                      value={this.state.expire}
-                      required
-                      // value={this.state.networkConfig.cpu}
-                    />
-    </div>)}
+                  </div>
+                  {this.state.genLicense && (
+                    <div className="col-md-4 form-input-group">
+                      <label>License Expiry After </label>
+                      <span className="help"> [in Months]</span>
+                      <input
+                        name="expire"
+                        type="number"
+                        placeholder="no of months"
+                        className="form-control"
+                        onChange={this.handleChanges.bind(this)}
+                        value={this.state.expire}
+                        required
+                        // value={this.state.networkConfig.cpu}
+                      />
+                    </div>
+                  )}
+                  </div>
+                  <br />
+                  <label>Client Data</label>
+                  <div className='row'>
+                      <div className='col-md-4 form-input-group'>
+                      <label> Clients Meta-data</label>
+                      <input
+                        name="clientMeta"
+                        type="text"
+                        placeholder="Some Note... *Optional "
+                        className="form-control"
+                        onChange={this.handleChanges.bind(this)}
+                        value={this.state.clientMeta}
+                        required
+                      />
+                      </div>
+                      <div className='form-input-group'>
+                        <label>Client Logo</label>
+                        <span className='help'> e.g URI</span>
+                        <input
+                        name='clientLogo'
+                        type="text"
+                        placeholder="https://...."
+                        className="form-control"
+                        onChange={this.handleChanges.bind(this)}
+                        value={this.state.clientLogo}
+                        required/>
+                      </div><div className='m-t-30'><CopyToClipboard text={this.state.clientLogo}><button className='btn btn-default'><i className='fa fa-copy'/></button></CopyToClipboard></div> 
                 </div>
               </div>
               <LaddaButton
-                disabled={(this.state.clientName.length > 0  && this.state.emailId.length>0 && this.state.phone.length>0) ? false : true}
+                disabled={this.state.clientName.length > 0 && this.state.emailId.length > 0 && this.state.phone.length > 0 ? false : true}
                 loading={this.state.createClient_formloading ? this.state.createClient_formloading : false}
                 data-size={S}
                 data-style={SLIDE_UP}
@@ -172,9 +202,9 @@ class ClientCreate extends Component {
       </div>
     );
   }
-
 }
 export default withTracker(() => {
-    return {
-    };
-  })(withRouter(ClientCreate));
+  return {};
+})(withRouter(ClientCreate));
+
+
