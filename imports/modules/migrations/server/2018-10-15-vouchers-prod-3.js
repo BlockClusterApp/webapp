317341d103b8d8ddb3a96e2d607c8f1b658c2f1e
saikat <saikatchakrabortty2@gmail.com>
2018-10-15 10:38:46 +0530

    new Versio added

---
+++ imports/modules/migrations/server/2018-10-15-vouchers-prod-3.js
@@ -0,0 +1,29 @@
+import Vouchers from '../../../collections/vouchers/voucher';
+const data = require('./data/voucher-prod-2');
+
+const deleteDummyVouchers = async vouchers => {
+  console.log('deleting existing voucher');
+  Vouchers.remove({
+    code: { $in: vouchers },
+  });
+
+  return true;
+};
+
+const voucherToUpdate = ['R7T552RU', 'X4ZQOFR9', 'X25UPRDM', 'O8XWYQTZ', '7CER1W5R', 'LBLEDF9T', 'UWCQYTJG', 'IC5YQR0I', 'O1IVDKBI', 'HIUU13D8'];
+
+const updateGivenDetails = async(voucherToUpdate)=>{
+  Vouchers.update({code:{$in:voucherToUpdate}},{
+    'discount.percent':false
+  },{multi:true});
+  return true;
+};
+Migrations.add({
+  version: 10,
+  up: function() {
+    Promise.all([deleteDummyVouchers(data), updateGivenDetails(voucherToUpdate)]);
+  },
+  down: function() {
+    Vouchers.remove({ code: { $in: data } });
+  },
+});
