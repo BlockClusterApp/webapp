faab6629c69da28b5e087aef2df048a2069eae6f
Jibin <jibinmathews7@gmail.com>
2018-10-29 12:22:18 +0530

    Adding winston daily file rotater

--- packages/logs/package.js
+++ packages/logs/package.js
@@ -1,20 +1,17 @@
 Package.describe({
   summary: 'Log consumer',
   version: '0.1.0',
-  name: 'blockcluster:logs'
+  name: 'blockcluster:logs',
 });
 
 Npm.depends({
-  'winston': '3.0.1'
+  winston: '3.0.1',
+  'winston-daily-rotate-file': '^3.4.0',
 });
 
-Package.onUse(function (api, where) {
+Package.onUse(function(api, where) {
   api.versionsFrom('METEOR@0.9.0');
-  api.addFiles('lib/main.js', [ 'server' ]);
+  api.addFiles('lib/main.js', ['server']);
 
-  api.export([
-    'ElasticLogger'
-  ], [
-    'server'
-  ]);
+  api.export(['ElasticLogger'], ['server']);
 });
