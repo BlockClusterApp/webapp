63681f8386f19aa3f2752b2f3e20f305c1d05ccb
Jibin <jibinmathews7@gmail.com>
2018-10-31 13:43:52 +0530

    daemon now requests local licence, support to wait for licence to boot

--- shell-scripts/docker-start.sh
+++ shell-scripts/docker-start.sh
@@ -2,9 +2,11 @@
 
 if [ -d ../licensing-microservice ];
 then
-  docker-compose up -f docker-compose.yml
+  docker-compose run start_dependencies
+  docker-compose up
   # folder exists
 else
+  docker pull 402432300121.dkr.ecr.us-west-2.amazonaws.com/licensing-micro:dev
   docker-compose up -f docker-compose.licence-built.yml
   # Folder does not exist
 fi
