bd63ab25e89666fe70a6c44648812969b2c40200
Jibin <jibinmathews7@gmail.com>
2019-01-23 13:56:45 +0530

    Adding indexes

--- imports/collections/walletTransactions/walletTransactions.js
+++ imports/collections/walletTransactions/walletTransactions.js
@@ -1,3 +1,43 @@
-import { Mongo } from "meteor/mongo";
+import { Mongo } from 'meteor/mongo';
+import { Meteor } from 'meteor/meteor';
 
-export const WalletTransactions = new Mongo.Collection("walletTransactions");
+const txn = new Mongo.Collection('walletTransactions');
+
+if (Meteor.isServer) {
+  txn._ensureIndex(
+    {
+      toWallet: 1,
+      fromAddress: 1,
+      txnId: 1,
+      type: 1,
+    },
+    {
+      unique: true,
+    }
+  );
+  txn._ensureIndex({
+    txnId: 1,
+  });
+  txn._ensureIndex({
+    txnId: 1,
+    type: 1,
+  });
+  txn._ensureIndex({
+    fromWallet: 1,
+    internalStatus: 1,
+    isInternalTxn: 1,
+    type: 1,
+  });
+  txn._ensureIndex({
+    toWallet: 1,
+    internalStatus: 1,
+    isInternalTxn: 1,
+    type: 1,
+  });
+  txn._ensureIndex({
+    fromWallet: 1,
+    internalStatus: 1,
+  });
+}
+
+export const WalletTransactions = txn;
