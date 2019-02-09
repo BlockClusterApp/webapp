c59fcbbc8e34215cea33e06282908f698c5c7879
Jibin <jibinmathews7@gmail.com>
2019-02-09 11:56:05 +0530

    Adding service host in env

--- imports/api/privatehive/server-communicator.js
+++ imports/api/privatehive/server-communicator.js
@@ -12,7 +12,7 @@ const packageDescriptor = grpc.loadPackageDefinition(packageDefinition);
 const { privatehive } = packageDescriptor;
 
 if (process.env.NODE_ENV === 'development') {
-  process.env.PRIVATEHIVE_SERVICE_GRPC_URL = process.env.PRIVATEHIVE_SERVICE_GRPC_URL || 'localhost:3010';
+  process.env.PRIVATEHIVE_SERVICE_GRPC_URL = process.env.PRIVATEHIVE_SERVICE_GRPC_URL || 'privatehive-dev.blockcluster.io';
 }
 
 if (!(process.env.PRIVATEHIVE_SERVICE_GRPC_URL || process.env.NO_PRIVATEHIVE)) {
